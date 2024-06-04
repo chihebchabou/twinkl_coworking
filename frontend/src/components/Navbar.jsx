cbafb4eaa37f8a017a6be533172c1d795c69a7ad
chihebchabou <chihebchaboutech@gmail.com>
2024-06-04 17:11:34 +0100

    coding session 04/06/2024 17:11

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(true);
+    const [nav, setNav] = useState(false);
 
     const handleNav = () => setNav(!nav)
     
@@ -20,10 +20,10 @@ const Navbar = () => {
         </ul>
 
         <div onClick={handleNav} className="block md:hidden">
-            {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+            {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
         </div>
 
-        <div className={`block md:hidden ${!nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%] top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"}`}>
+        <div className={`block md:hidden ${nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%] top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"}`}>
             <h1 className="w-full text-3xl font-bold uppercase text-deepSkyBlue m-4" >Twinkl.</h1>
             <ul className="uppercase">
                 <li className="p-4 border-b border-gray-600">Home</li>
